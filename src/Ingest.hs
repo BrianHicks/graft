96019df3c702c6aaaf9e9baafb5b04c85526b13e
Brian Hicks <brian@brianthicks.com>
2018-01-11 14:31:38 -0600

    type aliases for clarity

--- src/Ingest.hs
+++ src/Ingest.hs
@@ -14,18 +14,25 @@ import System.Directory.PathWalk (pathWalk)
 import System.FilePath (FilePath, combine, takeExtension)
 import System.FilePath.Glob (Pattern, match)
 
+-- | this is also Object, but too many type aliases :S
+type Subject = Text
+
+type Predicate = Text
+
+type FileInfo = (Subject, [(Predicate, Subject)])
+
 data Ingester = Ingester
   { forFiles :: Pattern
-  , parse :: FilePath -> IO (Text, [(Text, Text)])
+  , parse :: FilePath -> IO FileInfo
   }
 
 instance Show Ingester where
   show (Ingester forFiles _) = "Ingester for (" ++ show forFiles ++ ")"
 
-ingester :: Pattern -> (FilePath -> IO (Text, [(Text, Text)])) -> Ingester
+ingester :: Pattern -> (FilePath -> IO FileInfo) -> Ingester
 ingester = Ingester
 
--- ingest :: [Ingester] -> FilePath -> IO (Gr Text Text)
+-- ingest :: [Ingester] -> FilePath -> IO (Gr Subject Predicate)
 ingest :: [Ingester] -> FilePath -> IO ()
 ingest ingesters root = do
   pathWalk root <| \dir subdirs files -> do
@@ -33,20 +40,22 @@ ingest ingesters root = do
     matched <- ingesters |> mapM (ingestFiles fullPaths)
     case flattenNodesAndEdges matched of
       ([], []) -> pure ()
-      (nodes, edges) -> (mkGraph nodes edges :: Gr Text Text) |> prettyPrint
+      (nodes, edges) ->
+        (mkGraph nodes edges :: Gr Subject Predicate) |> prettyPrint
   -- 2a. getting the node names and relations for those ingesters whose `forFiles` match
   -- 3. convert that information to something I can make into a graph
   -- 4. return the graph, hooray!
   pure ()
 
-toNode :: Text -> LNode Text
+toNode :: Subject -> LNode Subject
 toNode label = (hash label, label)
 
-toEdge :: Text -> Text -> Text -> LEdge Text
+toEdge :: Subject -> Predicate -> Subject -> LEdge Predicate
 toEdge subject predicate object = (hash subject, hash object, predicate)
 
 flattenNodesAndEdges ::
-     [([LNode Text], [LEdge Text])] -> ([LNode Text], [LEdge Text])
+     [([LNode Subject], [LEdge Predicate])]
+  -> ([LNode Subject], [LEdge Predicate])
 flattenNodesAndEdges everything =
   foldr
     (\(newNodes, newEdges) (nodes, edges) ->
@@ -54,13 +63,13 @@ flattenNodesAndEdges everything =
     ([], [])
     everything
 
-ingestFiles :: [FilePath] -> Ingester -> IO ([LNode Text], [LEdge Text])
+ingestFiles :: [FilePath] -> Ingester -> IO ([LNode Subject], [LEdge Predicate])
 ingestFiles files ingester = do
   let interesting = filter (match (forFiles ingester)) files
   nodesAndEdges <- mapM (ingestFile ingester) interesting
   pure <| flattenNodesAndEdges nodesAndEdges
 
-ingestFile :: Ingester -> FilePath -> IO ([LNode Text], [LEdge Text])
+ingestFile :: Ingester -> FilePath -> IO ([LNode Subject], [LEdge Predicate])
 ingestFile (Ingester _ getInfo) filepath = do
   (subject, relations) <- getInfo filepath
   -- edges
